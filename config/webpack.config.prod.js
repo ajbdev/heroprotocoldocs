88e36a66b8e15221daf9f8b8ccaff9acf858e62c
Andy Baird <andybaird@gmail.com>
2017-12-11 22:04:37 -0600

    Add markdown loader to webpack config for prod

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -153,6 +153,17 @@ module.exports = {
               compact: true,
             },
           },
+          {
+            test: /\.md$/,
+            use: [
+              {
+                  loader: "html-loader"
+              },
+              {
+                  loader: "markdown-loader"
+              }
+            ]
+          },
           // The notation here is somewhat confusing.
           // "postcss" loader applies autoprefixer to our CSS.
           // "css" loader resolves paths in CSS and adds assets as dependencies.
